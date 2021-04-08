83d42d0649ac76f4ff612d1f9eef540c679ab27b
Juan Daniel <pericles_navarro@hotmail.com>
2021-04-08 12:05:37 -0500

    Create OptionalsValues.swift

---
+++ Optionals/OptionalsValues.swift
@@ -0,0 +1,5 @@
+// Force Unwrapping ---------------------------
+var prueba : Int? //Declaro una variable Opcional con el simbolo "?", y toma como valor por defalut "nil"
+prueba = 2 // Al momento de asignarle un valor este se oculta. Esta en una caja
+print("\(prueba!)") //Para desempaquetarlo de esa caja, usamos el signo "!"
+//Aprendido de: https://kodigoswift.com/swift-opcionales/
