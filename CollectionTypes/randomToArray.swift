bb9325f247a1cda6bc545a712a953a048dc38a9a
Juan Daniel <pericles_navarro@hotmail.com>
2021-04-08 11:36:56 -0500

    Create randomToArray.swift

---
+++ CollectionTypes/randomToArray.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+// Objeto Random - randomElement ; Int.random(0...VALUE) || Int.random(0..<VALUE)
+// Form 1
+let listita = ["Juan","Edith","Paula","Stefania"]
+let elegido = listita.randomElement()
+print(elegido ?? "Error")
+// Form 2
+let indice = Int.random(0...listita.count)
+print(listita[indice])
+
+// Al igual que 'Int', se puede aplicar para los otros tipos de datos, Float, Double, Booleans (en booleans solo abran dos posibilidades)
