68847aad9999f5afc6e477933bfeae21717c3a24
Juan Daniel <pericles_navarro@hotmail.com>
2021-04-23 19:05:29 -0500

    Create URLSession.swift

---
+++ URL/URLSession.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lbl_ubicacion: UILabel!
+    @IBOutlet weak var lbl_resultado: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func btn_request(_ sender: UIButton) {
+        
+        // Valor que identifica la ubicacion de recursos en un servidor web o la ruta a un archivo local
+        let api = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=b3ceaef88d93238d6f3c0cfe6212bf48&q=guayaquil"
+        if let url = URL(string: api){
+            
+            // Objeto que coordina tareas de transferencia de datos en la red
+            // Se usa instancias de URLSession para crear una o mas instancias de URLSessiontASK
+            let urlsession = URLSession(configuration: .default)
+            
+            // Tareas como enviar y traer datos desde la app, descargar, subir archivos remotos
+            // DENTRO DE UNA SESION SE PUEDE ASOCIAR VARIAS TAREAS, QUE SON REPRESENTADAS POR SOLICITUDES URLs
+            let sessionTask = urlsession.dataTask(with: url, completionHandler: {(data,response,error) in
+                if error != nil {
+                    print(error!)
+                }
+                if let datasession = data{
+                    
+                    // DataTask request a resource, returning the server's response as one or more NSData objects in memory
+                    print(datasession)
+                    print("Datos recibidos")
+                }
+            })
+            
+            // Reanuda la tarea si ha sido suspendida
+            // TAREAS NUEVAS INICIALIZADAS, EMPIEZAN EN ESTADO SUSPENDIDO, POR LO QUE HAY QUE LLAMAR A ESTE METODO
+            sessionTask.resume()
+        }
+    }
+}
